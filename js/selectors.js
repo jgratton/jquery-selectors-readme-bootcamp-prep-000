acdfa28c2d8031a536bf7fba1930dd263d4e2398
John Gratton <jgratton@gmail.com>
2018-03-30 03:44:04 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-  // code to be executed goes here
-});
+function paragraphSelector() {
+  return $("p");
+}
+
+function lastImageSelector() {
+  return $("img").last();
+}
+
+function ninjaBabySelector() {
+  return $("img#baby-ninja");
+}
+
+function divSelector() {
+  return $("div.pics");
+}
+
+function firstListItem() {
+  return $("ul li").first();
+}
